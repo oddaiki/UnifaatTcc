dc599cb4d55d83a636794d6420607694dfa4954c
Bowie <viniciusj.ferreira12@gmail.com>
2023-12-13 21:54:40 -0300

    Adicionar arquivos de projeto.

---
+++ UnifaatTcc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UnifaatTcc.Models;
+
+namespace UnifaatTcc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Vinicius Jose Ferreira";
+            home.Email = "viniciusj.trx20@gmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UnifaatTcc/Models/HomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UnifaatTcc.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
